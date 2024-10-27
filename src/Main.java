f03507307d7a4528766233973b514104d66966f0
DanteVid <daniel.ruiz@estudiantesunibague.edu.co>
2024-10-27 09:24:39 -0500

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import userInterface.PestanaLogin;
+
+public class Main {
+    public static void main(String[] args) {
+        PestanaLogin pestanaLogin = new PestanaLogin();
+        pestanaLogin.setVisible(true);
+        pestanaLogin.pack();
+        pestanaLogin.setLocationRelativeTo(null);
+    }
+}
